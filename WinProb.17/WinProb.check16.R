7ef4f3cf266a595730ac9440095a14b3d01b805d
MICHAEL LOPEZ <mlopez1@skidmore.edu>
2017-03-08 12:30:35 -0500

    updated code

--- WinProb.17/WinProb.check16.R
+++ WinProb.17/WinProb.check16.R
@@ -6,6 +6,7 @@ library(tidyr)
 library(nflscrapR)
 library(RColorBrewer)
 library(readr)
+library(readxl)
 library(GGally)
 
 
@@ -82,7 +83,7 @@ cmu.wp <- filter(cmu.wp, !is.na(Away.WP.pre), as.numeric(as.character(qtr)) < 5)
 
 
 ## PFR model
-pfr.wp <- read.csv("https://raw.githubusercontent.com/statsbylopez/BlogPosts/master/WinProb.17/PFR.csv")
+pfr.wp <- read.csv("https://raw.githubusercontent.com/statsbylopez/BlogPosts/master/WinProb.17/pfr.csv")
 pfr.wp <- filter(pfr.wp, !is.na(Win.),  Quarter!="OT") %>% 
   mutate(Pats.wp = lag(1 - Win./100, 0), qtr = (as.numeric(as.character(Quarter)))) %>%
   select(-Detail) %>%
@@ -92,7 +93,7 @@ pfr.wp <- filter(pfr.wp, !is.na(Win.),  Quarter!="OT") %>%
          Pats.delta = Pats.wp - lag(Pats.wp, 1))
 
 
-## Lock-Nettleton model
+## Lock-Nettleton model in the Super Bowl
 LockNett.Pats <- LockNett.test %>%
   filter(gid == 4523) %>%
   mutate(Pats.wp = ifelse(off == "NE", winprob.off, 1 - winprob.off), 
@@ -104,14 +105,8 @@ LockNett.Pats <- LockNett.test %>%
 
 
 ## Gambletron
-Gambletron.wp <- read.csv("~/Dropbox/BlogPosts/BlogPosts/super_bowl_li.csv")
-
-## ESPN
-espn.wp <- read.csv("~/Dropbox/BlogPosts/BlogPosts/super_bowl_li.csv")
-
-
-## Halftime is 8:03 to 8:35, so drop those
-## Overtime starts at 10:14, so drop those
+Gambletron.wp <- read.csv("https://raw.githubusercontent.com/statsbylopez/BlogPosts/master/WinProb.17/Gambletron.csv")
+## Halftime is 8:03 to 8:35, so drop those, Overtime starts at 10:14, so drop those
 Gambletron.wp <- mutate(Gambletron.wp, Pats.wp = ne_win_prob, row.index = 1:n()) %>% 
   mutate(quarter = ifelse(row.index < 192, 1, 
                           ifelse(row.index < 341, 2, 
@@ -121,46 +116,64 @@ Gambletron.wp <- mutate(Gambletron.wp, Pats.wp = ne_win_prob, row.index = 1:n())
   group_by(quarter) %>% 
   mutate(seconds = (900 - 1:n()/n()*900) + 900*(4-quarter)) %>% ungroup()
 
-ann_text <- data.frame(x = c(450, 450, 450, 450, 450), 
-                       y1 = c(0.90, 0.85, 0.8, 0.75, 0.7), 
-                       y2 = c(900, 850, 800, 750, 700),
-                       lab = c("Pro Football Reference", "nflscrapr", 
-                               "Lock & Nettleton", "Gambletron", "phdFootball"), 
-                       int = c(min(pfr.wp$Pats.wp), min(cmu.wp$Pats.wp), min(LockNett.Pats$Pats.wp), 
-                               min(Gambletron.wp$Pats.wp), min(phd.wp$Pats.wp)), 
+
+## ESPN accessed remotely
+espn.wp <- read.csv("~/Dropbox/BlogPosts/BlogPosts/WinProb.17/espn.csv")
+espn.wp <- filter(espn.wp,  QUARTER!="OVERTIME") %>% 
+  mutate(Pats.wp = ifelse(TEAM == "New England Patriots", TEAM_WP_START, 1 - TEAM_WP_START), 
+         QUARTER = as.numeric(as.character(QUARTER))) %>%
+  separate(CLOCK_TIME, c("min", "sec"), sep = ":", remove=FALSE) %>% 
+  mutate(min = as.numeric(as.character(min)), sec = as.numeric(as.character(sec)), 
+         seconds = 15*60*(4-QUARTER) + (60*min) + sec, 
+         Pats.delta = lead(Pats.wp, 1) - Pats.wp)
+
+
+ann_text <- data.frame(x = c(450, 450, 450, 450, 450, 450), 
+                       y1 = c(0.95, 0.90, 0.85, 0.8, 0.75, 0.7), 
+                       y2 = c(950, 900, 850, 800, 750, 700),
+                       lab = c("Pro Football Reference", "ESPN", "phdFootball", "nflscrapr", 
+                               "Lock & Nettleton", "Gambletron"), 
+                       int = c(min(pfr.wp$Pats.wp), min(espn.wp$Pats.wp), min(phd.wp$Pats.wp), 
+                               min(cmu.wp$Pats.wp), min(LockNett.Pats$Pats.wp), min(Gambletron.wp$Pats.wp)), 
                        seconds = c(pfr.wp$seconds[which.min(pfr.wp$Pats.wp)], 
+                                   espn.wp$seconds[which.min(espn.wp$Pats.wp)], 
+                                   phd.wp$seconds[which.min(phd.wp$Pats.wp)], 
                                    cmu.wp$seconds[which.min(cmu.wp$Pats.wp)], 
                                    LockNett.Pats$seconds[which.min(LockNett.Pats$Pats.wp)], 
-                                   Gambletron.wp$seconds[which.min(Gambletron.wp$Pats.wp)], 
-                                   phd.wp$seconds[which.min(phd.wp$Pats.wp)]))
-cols <- brewer.pal(5,"Set1")
+                                   Gambletron.wp$seconds[which.min(Gambletron.wp$Pats.wp)]))
+cols <- c(brewer.pal(5,"Set1"), "#A6611A")
 
-ggplot(LockNett.Pats, aes(3600-seconds, Pats.wp)) + geom_path(colour = cols[3]) + 
+cols <- brewer.pal(6, "Set1")
+
+ggplot(LockNett.Pats, aes(3600-seconds, Pats.wp)) + geom_path(colour = cols[5]) + 
   scale_x_continuous("",labels = c("Q1", "Q2", "Q3", "Q4", "End of Reg"), 
                      breaks = c(0, 900, 1800, 2700, 3600)) + 
   scale_y_continuous(lim = c(0, 1), "", labels=scales::percent) + 
-  geom_path(data = cmu.wp, colour = cols[2]) + 
   geom_text(data = ann_text,aes(x = x, y = y1, label = lab), 
             colour = cols) +
   geom_path(data = pfr.wp, colour = cols[1]) + 
-  geom_path(data = Gambletron.wp, colour = cols[4]) + 
-  geom_path(data = phd.wp, colour = cols[5]) + 
+  geom_path(data = espn.wp, colour = cols[2]) +
+  geom_path(data = phd.wp, colour = cols[3]) +  
+  geom_path(data = cmu.wp, colour = cols[4]) + 
+  geom_path(data = Gambletron.wp, colour = cols[6]) + 
   labs(title = "Super Bowl win probability") + 
-  theme_bw(14)
+  theme_grey(14)
 
 
 p <- ggplot(LockNett.Pats, aes(3600-seconds, 1/(Pats.wp/(1-Pats.wp)))) + 
-  geom_path(colour = cols[3]) + 
+  geom_path(colour = cols[5]) + 
   scale_x_continuous("",labels = c("Q2", "Q3", "Q4", "End of Reg"), 
-                     breaks = c(900, 1800, 2700, 3600), lim = c(900, 3600)) + 
+                     breaks = c(900, 1800, 2700, 3600), lim = c(1800, 3600)) + 
   scale_y_continuous("", breaks = c(20, 100, 200, 500, 1000), 
                      labels = c("20:1", "100:1", "200:1", "500:1", "1000:1")) + 
   geom_path(data = cmu.wp, colour = cols[2]) + 
-  geom_text(data = ann_text, aes(x = x + 900, y = y2, label = lab), 
+  geom_text(data = ann_text, aes(x = x + 1500, y = y2, label = lab), 
             colour = cols) +
   geom_path(data = pfr.wp, colour = cols[1]) + 
-  geom_path(data = Gambletron.wp, colour = cols[4]) + 
-  geom_path(data = phd.wp, colour = cols[5]) + 
+  geom_path(data = espn.wp, colour = cols[2]) +
+  geom_path(data = phd.wp, colour = cols[3]) +  
+  geom_path(data = cmu.wp, colour = cols[4]) + 
+  geom_path(data = Gambletron.wp, colour = cols[6]) + 
   labs(title = "Super Bowl win odds (New England)")
 
 
@@ -170,7 +183,7 @@ for (i in ints){
                         xend = 3600 - ann_text$seconds[i], yend = 1/(ann_text$int/(1-ann_text$int))[i],
                         colour = cols[i], lty = 2)   
 }
-p
+p + theme_grey(14)
 
 
 
@@ -184,23 +197,23 @@ el1 <- LockNett.Pats %>% filter(type == "RUSH"|type == "PASS", Pats.delta < 0.3)
 el2 <- pfr.wp %>% group_by(seconds) %>% count() %>% filter(n == 1) 
 el3 <- cmu.wp %>% group_by(seconds) %>% count() %>% filter(n == 1) 
 el4 <- phd.wp %>% group_by(seconds) %>% count() %>% filter(n == 1) 
+el5 <- espn.wp %>% group_by(seconds) %>% count() %>% filter(n == 1) 
 
-el.all <- Reduce(intersect, list(el1$seconds, el2$seconds, el3$seconds, el4$seconds))
+el.all <- Reduce(intersect, list(el1$seconds, el2$seconds, el3$seconds, el4$seconds, el5$seconds))
 
-elig.1 <- cmu.wp$seconds[cmu.wp$seconds %in% elig.plays$seconds]
+
 winprob.Pats.all <- data.frame(seconds = rev(el.all), 
                                `Lock_Nettleton` = LockNett.Pats$Pats.delta[LockNett.Pats$seconds %in% el.all], 
                                `Pro_Football_Reference` = pfr.wp$Pats.delta[pfr.wp$seconds %in% el.all], 
                                `nflscrapR` = cmu.wp$Pats.delta[cmu.wp$seconds %in% el.all], 
-                               `PhDfootball` = phd.wp$Pats.delta[phd.wp$seconds %in% el.all])
+                               `PhDfootball` = phd.wp$Pats.delta[phd.wp$seconds %in% el.all], 
+                               `ESPN` = espn.wp$Pats.delta[espn.wp$seconds %in% el.all])
 
-winprob.Pats.all %>% mutate(agree.all = (Lock_Nettleton > 0) & (Pro_Football_Reference) > 0 & (nflscrapR > 0) & (PhDfootball > 0) | 
-                              (Lock_Nettleton < 0) & (Pro_Football_Reference) < 0 & (nflscrapR < 0) & (PhDfootball < 0) ) %>%
+winprob.Pats.all %>% mutate(agree.all = (Lock_Nettleton > 0) & (Pro_Football_Reference) > 0 & (nflscrapR > 0) & (PhDfootball > 0) & (ESPN > 0)| 
+                              (Lock_Nettleton < 0) & (Pro_Football_Reference < 0) & (nflscrapR < 0) & (PhDfootball < 0) & (ESPN < 0) ) %>%
   count(agree.all)
 
 
-
-round(filter(winprob.Pats.all, PhDfootball < 0.01, Pro_Football_Reference > 0.05), 2)
 
 library(GGally)
 
@@ -221,7 +234,7 @@ my_fn2 <- function(data, mapping, ...){
 
 ggpairs(winprob.Pats.all[,2:ncol(winprob.Pats.all)], lower = list(continuous = my_fn), diag = list(continuous = my_fn2)) + 
   labs(title = "New England's win probability added per play in Super Bowl 51", 
-       subtitle = "Rushes and passes only (either team on offense)") 
+       subtitle = "Rushes and passes only (either team on offense)")  + theme_grey(14)
 
 
 
