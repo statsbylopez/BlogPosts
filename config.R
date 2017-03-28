bdb8c8c41bfe53e8ccc6c72df7005f69cef08855
Michael Lopez <mlopez1@skidmore.edu>
2017-03-27 21:23:26 -0400

    gfiles

--- config.R
+++ config.R
@@ -4,12 +4,14 @@ if (Sys.info()["user"] == "Brian") {
   # For Brian
   constants <- "C:/Users/Brian/Dropbox/Sim/MJL_code/MLconstants/"
   output <- "C:/Users/Brian/Dropbox/Sim/MJL_code/Outcomes/"
+  gamefiles <- "C:/Users/Brian/Dropbox/sim/MJL_code/12-16-2016/Model/gamedata/"
 }
 
 if (Sys.info()["user"] == "mlopez1") {
   # For Mike
   constants <- "~/Dropbox/Sim/MJL_code/MLconstants/"
   output <- "~/Dropbox/Sim/MJL_code/Outcomes/"
+  gamefiles <- "~/Dropbox/sim/MJL_code/12-16-2016/Model/gamedata/"
 }
 
 
