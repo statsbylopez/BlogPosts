256b8d095de2ba401b97f68e83ee636aca3f2d83
Michael Lopez <mlopez1@skidmore.edu>
2017-03-27 21:17:20 -0400

    config

---
+++ config.R
@@ -0,0 +1,20 @@
+# local configuration hack
+
+if (Sys.info()["user"] == "Brian") {
+  # For Brian
+  constants <- "C:/Users/Brian/Dropbox/Sim/MJL_code/MLconstants/"
+  output <- "C:/Users/Brian/Dropbox/Sim/MJL_code/Outcomes/"
+}
+
+if (Sys.info()["user"] == "mlopez1") {
+  # For Mike
+  constants <- "~/Dropbox/Sim/MJL_code/MLconstants/"
+  output <- "~/Dropbox/Sim/MJL_code/Outcomes/"
+}
+
+library(knitr)
+library(readxl)
+library(readr)
+library(profvis)
+library(dplyr)
+library(doParallel)
