9f443a893be3a230f3973705cd09ee85284619d9
Michael Lopez <mlopez1@skidmore.edu>
2018-02-13 11:52:29 -0500

    Update config.R

--- config.R
+++ config.R
@@ -1,49 +1,54 @@
 # local configuration hack
 
-if (Sys.info ()["user"] == "doherbr") {
-  # For Brian-JHK
-  constants <- "C:/Users/doherbr/Desktop/Dropbox/Sim/MJL_code/MLconstants/"
-  output <- "C:/Users/doherbr/Desktop/Dropbox/Sim/MJL_code/Outcomes/"
-  gamefiles <- "C:/Users/doherbr/Desktop/Dropbox/sim/MJL_code/12-16-2016/Model/gamedata/"
-  gamedates <- "C:/Users/doherbr/Desktop/Dropbox/sim/MJL_code/12-16-2016/Model/constants/"
-  ModelLocation <- "C:/Users/doherbr/Desktop/Dropbox/Sim/MJL_code/12-16-2016/Model/"
-  mljcode <- "C:/Users/doherbr/Desktop/Dropbox/Sim/MJL_code/"
+if (Sys.info()["user"] == "Brian") {
+  # For Brian
+  constants <- "C:/Users/Brian/Dropbox/Constants/"
+  output <- "C:/Users/Brian/Dropbox/Sim/2018 Outcomes/"
+  gamefiles <- "C:/Users/Brian/Dropbox/sim/Model/gamedata/"
+  gamedates <- "C:/Users/Brian/Dropbox/sim/Model/constants/"
+  ModelLocation <- "C:/Users/Brian/Dropbox/Sim/Model/"
+  mljcode <- "C:/Users/Brian/Dropbox/Sim/"
 }
 
-if (Sys.info ()["user"] == "Brian") {
-  # For Brian
-  constants <- "C:/Users/Brian/Desktop/Dropbox/Sim/MJL_code/MLconstants/"
-  output <- "C:/Users/Brian/Desktop/Dropbox/Sim/MJL_code/Outcomes/"
-  gamefiles <- "C:/Users/Brian/Desktop/Dropbox/sim/MJL_code/12-16-2016/Model/gamedata/"
-  gamedates <- "C:/Users/Brian/Desktop/Dropbox/sim/MJL_code/12-16-2016/Model/constants/"
-  ModelLocation <- "C:/Users/Brian/Desktop/Dropbox/Sim/MJL_code/12-16-2016/Model/"
-}
 if (Sys.info()["user"] == "mlopez1") {
   # For Mike
-  constants <- "~/Dropbox/Sim/MJL_code/MLconstants/"
-  output <- "~/Dropbox/Sim/MJL_code/Outcomes/"
-  gamefiles <- "~/Dropbox/sim/MJL_code/12-16-2016/Model/gamedata/"
-  gamedates <- "~/Dropbox/sim/MJL_code/12-16-2016/Model/constants/"
-  ModelLocation <- "~/Dropbox/Sim/MJL_code/12-16-2016/Model/"
+  constants <- "~/Dropbox/Sim/Constants/"
+  output <- "~/Dropbox/Sim/2018 Outcomes/"
+  gamefiles <- "~/Dropbox/sim/Model/gamedata/"
+  gamedates <- "~/Dropbox/sim/Model/constants/"
+  ModelLocation <- "~/Dropbox/Sim/Model/"
+  mljcode <- "~/Dropbox/Sim/"
 }
 
 
 if (Sys.info()["user"] == "elopez") {
   # For Erin
-  constants <- "C:/Users/elopez/Dropbox/Sim/MJL_code/MLconstants/"
-  output <- "C:/Users/elopez/Dropbox/Sim/MJL_code/Outcomes/"
-  gamefiles <- "C:/Users/elopez/Dropbox/Sim/MJL_code/12-16-2016/Model/gamedata/"
-  gamedates <- "C:/Users/elopez/Dropbox/Sim/MJL_code/12-16-2016/Model/constants/"
-  ModelLocation <- "C:/elopez/Dropbox/Sim/MJL_code/12-16-2016/Model/"
+  constants <- "C:/Users/elopez/Constants/"
+  output <- "C:/Users/elopez/Dropbox/2018 Outcomes/"
+  gamefiles <- "C:/Users/elopez/Dropbox/Sim/Model/gamedata/"
+  gamedates <- "C:/Users/elopez/Dropbox/Sim/Model/constants/"
+  ModelLocation <- "C:/elopez/Dropbox/Sim/Model/"
+  mljcode <- "C:/elopez/Dropbox/Sim/"
 }
 
 if (Sys.info()["user"] == "jenkins") {
   # For Apple Sim Box
-  constants <- "~/Dropbox/Sim/MJL_code/MLconstants/"
-  output <- "~/Dropbox/Sim/MJL_code/Outcomes/"
-  gamefiles <- "~/Dropbox/sim/MJL_code/12-16-2016/Model/gamedata/"
-  gamedates <- "~/Dropbox/sim/MJL_code/12-16-2016/Model/constants/"
-  ModelLocation <- "~/Dropbox/Sim/MJL_code/12-16-2016/Model/"
+  constants <- "~/Dropbox/Sim/Constants/"
+  output <- "~/Dropbox/Sim/2018 Outcomes/"
+  gamefiles <- "~/Dropbox/sim/Model/gamedata/"
+  gamedates <- "~/Dropbox/sim/Model/constants/"
+  ModelLocation <- "~/Dropbox/Sim/Model/"
+  mljcode <- "~/Dropbox/Sim/"
+}
+
+if (Sys.info()["user"] == "doherbr") {
+  # For Brian-JHK
+  constants <- "C:/Users/doherbr/Dropbox/Constants/"
+  output <- "C:/Users/doherbr/Dropbox/Sim/2018 Outcomes/"
+  gamefiles <- "C:/Users/doherbr/Dropbox/sim/Model/gamedata/"
+  gamedates <- "C:/Users/doherbr/Dropbox/sim/Model/constants/"
+  ModelLocation <- "C:/Users/doherbr/Dropbox/Sim/Model/"
+  mljcode <- "C:/Users/doherbr/Dropbox/Sim/"
 }
 
 
