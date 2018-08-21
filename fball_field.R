e0ea3ccf246a90023f949f1959771bbf1edd04b1
Michael Lopez <mlopez1@skidmore.edu>
2018-08-21 17:28:58 -0400

    Update fball_field.R

--- fball_field.R
+++ fball_field.R
@@ -1,5 +1,4 @@
 
-
 theme.football <- function(){
   theme(panel.grid.minor = element_blank(), 
         axis.text.y = element_text(angle=270, hjust=0.5), 
@@ -9,28 +8,34 @@ theme.football <- function(){
         axis.ticks = element_blank()) 
 }
 
+xlim <- (160/3)/2
+hash.width <- 3.3
+hash.x <- (xlim + hash.width)/2
 
+df.hash <- expand.grid(x = c(-1*xlim, -1*hash.width, hash.width, xlim), y = (0:100))
+df.hash <- df.hash %>% filter(!(floor(y %% 5) == 0))
 base.football <- ggplot() + xlab("") + ylab("") + 
   theme_minimal() +
-  annotate("segment", x = c(-26.6, -26.6, 26.6, 26.6), 
+  annotate("segment", x = c(-1*xlim, -1*xlim, xlim, xlim), 
            y = c(-10, 110, 110, -10), 
-           xend = c(-26.6, 26.6, 26.6, -26.6), 
+           xend = c(-1*xlim, xlim, xlim, -1*xlim), 
            yend = c(110, 110, -10, -10), colour = "black") + 
-  annotate("segment", x = c(-3.1, 3.1), 
-           y = c(0, 0), 
-           xend =  c(-3.1, 3.1), 
-           yend = c(100, 100), colour = "black", lty = 2, alpha = 0.5) + 
-  annotate("segment", x = rep(-26.6, 11), 
-           y = seq(0, 100, by = 10), 
-           xend =  rep(26.6, 11), 
-           yend = seq(0, 100, by = 10)) +
-  annotate("text", x = rep(-20.5, 9), y = seq(10, 90, by = 10), 
-           label = c(seq(10, 50, by = 10),rev(seq(10, 40, by = 10))), 
-           angle = 270, size = 5) + 
-  annotate("text", x = rep(20.5, 9), y = seq(10, 90, by = 10), 
-           label = c(seq(10, 50, by = 10),rev(seq(10, 40, by = 10))), 
-           angle = 90, size = 5) + 
+  #geom_point(data = df.hash, aes(x, y), pch = 1) + 
+  annotate("text", x = df.hash$x[df.hash$x < 0], y = df.hash$y[df.hash$x < 0], label = "_", hjust = 0, vjust = -0.2) + 
+  annotate("text", x = df.hash$x[df.hash$x > 0], y = df.hash$y[df.hash$x > 0], label = "_", hjust = 1, vjust = -0.2) + 
+  annotate("segment", x = rep(-1*xlim, 21), 
+           y = seq(0, 100, by = 5), 
+           xend =  rep(xlim, 21), 
+           yend = seq(0, 100, by = 5)) +
+  annotate("text", x = rep(-1*hash.x, 11), y = seq(0, 100, by = 10), 
+           label = c("G   ", seq(10, 50, by = 10), rev(seq(10, 40, by = 10)), "   G"), 
+           angle = 270, size = 4) + 
+  annotate("text", x = rep(hash.x, 11), y = seq(0, 100, by = 10), 
+           label = c("   G", seq(10, 50, by = 10), rev(seq(10, 40, by = 10)), "G   "), 
+           angle = 90, size = 4) + 
   scale_y_continuous("", breaks = NULL, lim = c(-10, 110)) + 
-  scale_x_continuous("", breaks = NULL, lim = c(-26.6, 26.6)) + 
-  annotate("rect", xmin=-26.6, xmax=26.6, ymin=-10, ymax=110, fill="palegreen", alpha=0.1)  + 
+  scale_x_continuous("", breaks = NULL, lim = c(-1*xlim, xlim)) + 
+  annotate("rect", xmin=-1*xlim, xmax=xlim, ymin=-10, ymax=110, fill="palegreen", alpha=0.1)  + 
   theme.football()
+
+base.football
