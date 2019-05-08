84029f5be4b159012073652f3d76c8e82e5c3fac
Michael Lopez <mlopez1@skidmore.edu>
2019-05-08 08:23:36 -0500

    Update scrapr-data.R

--- scrapr-data.R
+++ scrapr-data.R
@@ -1,15 +1,15 @@
-
+## Grab the nflscrapr data so that it's in one data frame
 
 scrapr.fun <- function(year){
   file.read <- paste0("https://raw.githubusercontent.com/ryurko/nflscrapR-data/master/play_by#_play_data/regular_season/reg_pbp_",year,".csv")
   df.scrapr.temp <- suppressMessages(read_csv(file.read))
   print(year)
-  df.scrapr.1 <- df.scrapr.temp %>% select(home_team, game_date, play_id, game_id, desc, #half_seconds_remaining, game_seconds_remaining, down, ydstogo, yardline_100, play_type, #yards_gained, total_home_score, total_away_score, touchdown, pass_touchdown, 
-                                           rush_touchdown, penalty, penalty_yards, posteam, #fumble_recovery_1_team) 
+  df.scrapr.1 <- df.scrapr.temp %>% select(home_team, game_date, play_id, game_id, desc, half_seconds_remaining, game_seconds_remaining, 
+                                           down, ydstogo, yardline_100, play_type, yards_gained, total_home_score, total_away_score, 
+                                           touchdown, pass_touchdown, rush_touchdown, penalty, penalty_yards, posteam, fumble_recovery_1_team) 
   return(df.scrapr.1)
 }
 
-df.scrapr.temp %>% filter(!is.na(fumble_recovery_1_team)) %>% sample_n(3) %>% #print.data.frame()
 
 
 df.scrapr <- lapply(2009:2018, scrapr.fun)
