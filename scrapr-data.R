cd13ba72bf60bc3bb068cb4c219e2fff8a222858
Michael Lopez <mlopez1@skidmore.edu>
2019-05-09 13:35:00 -0400

    Update scrapr-data.R
    added function for play sampling

--- scrapr-data.R
+++ scrapr-data.R
@@ -16,3 +16,46 @@ df.scrapr <- lapply(2009:2018, scrapr.fun)
 scrapr.plays <- bind_rows(df.scrapr)
 
 write_csv(scrapr.plays, "scrapr_plays.csv")
+
+
+### Function to sample plays
+
+sample.rp.drive.needs.TD <- function(down, yards.to.go, yards.from.own.goal) {
+  
+  down.original <- down
+  if (yards.from.own.goal <= 5){yards.from.own.goal <- 5}
+  if (down == 4 & yards.to.go >= 3){down <- 3}
+  if (yards.to.go >= 20){yards.to.go <- 20}
+  
+  if (yards.from.own.goal <= 70) {
+      data.RP <- filter(df.scrimmage, yfog <= yards.from.own.goal + 3, 
+                        yfog >= yards.from.own.goal - 3,
+                        down == down, ydstogo == yards.to.go)
+    } else if (yards.from.own.goal <= 90) {
+      data.RP <- filter(df.scrimmage, yfog >= yards.from.own.goal - 2,
+                        yfog <= yards.from.own.goal + 2,
+                        down == down, ydstogo == yards.to.go)
+    } else if (yards.from.own.goal <= 97) {
+      data.RP <- filter(df.scrimmage, yfog >= yards.from.own.goal - 1,
+                        yfog <= yards.from.own.goal + 1,
+                        down == down, ydstogo == yards.to.go)
+    } else {
+      data.RP <- filter(df.scrimmage, yfog == yards.from.own.goal,
+                        down == down, ydstogo == yards.to.go)
+    }
+    sim.RP <- sample_n(data.RP, 1)
+    yds.gained <- sim.RP$yards_gained
+    new.yfog <- yards.from.own.goal + yds.gained
+    new.down <- ifelse(yds.gained >= yards.to.go, 1, down.original + 1)
+    new.distance <- ifelse(yds.gained >= yards.to.go & new.yfog <= 90, 10, 
+                           ifelse(yds.gained >= yards.to.go & new.yfog > 90, 100-new.yfog, 
+                                  yards.to.go - yds.gained))
+    if (new.distance <= 0){new.distance <- 1} 
+    if (new.yfog >= 100){new.yfog <- 99}
+    keep.drive <- data.frame(down.original, yards.to.go, yards.from.own.goal, 
+                             yds.gained, play = sim.RP$desc, new.yfog, new.down, new.distance, 
+                             is.turnover = sim.RP$is.turnover, 
+                             is.td.offense = sim.RP$is.td.offense, 
+                             end.drive = new.down > 4 | sim.RP$is.turnover | sim.RP$is.td.offense)
+    return(keep.drive)
+}  
