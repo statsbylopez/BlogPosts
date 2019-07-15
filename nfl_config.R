6e74aecac38f89800486be4346cd5005b60b3305
Michael Lopez <mlopez1@skidmore.edu>
2019-07-15 08:43:00 -0400

    Update nfl_config.R

--- nfl_config.R
+++ nfl_config.R
@@ -6,11 +6,11 @@ if (Sys.info ()["user"] == "Michael.Lopez") {
 }
 
 if (Sys.info ()["user"] == "jack.persampire") {
- ## for Mike
- base <- "C:/Users/Michael.Lopez/OneDrive - National Football League/Football Ops Analysis/"
+ ## for Jack
+ base <- "C:/Users/jack.persampire/National Football League/Lopez, Michael - Football Ops Analysis/"
 }
 
 if (Sys.info ()["user"] == "sean.mckee") {
- ## for Mike
+ ## for Sean
  base <- "C:/Users/Michael.Lopez/OneDrive - National Football League/Football Ops Analysis/"
 }
