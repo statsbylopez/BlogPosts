ab29606209005c6640488364edd002e10071a660
Michael Lopez <mlopez1@skidmore.edu>
2019-07-15 08:37:59 -0400

    Create nfl_config.R

---
+++ nfl_config.R
@@ -0,0 +1,16 @@
+# local configuration hack
+
+if (Sys.info ()["user"] == "Michael.Lopez") {
+ ## for Mike
+ base <- "C:/Users/Michael.Lopez/OneDrive - National Football League/Football Ops Analysis/"
+}
+
+if (Sys.info ()["user"] == "jack.persampire") {
+ ## for Mike
+ base <- "C:/Users/Michael.Lopez/OneDrive - National Football League/Football Ops Analysis/"
+}
+
+if (Sys.info ()["user"] == "sean.mckee") {
+ ## for Mike
+ base <- "C:/Users/Michael.Lopez/OneDrive - National Football League/Football Ops Analysis/"
+}
