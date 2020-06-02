a6dd4be4b7e4a77d00b1502ea3706888fad3915b
Michael Lopez <mlopez1@skidmore.edu>
2020-06-01 21:28:39 -0400

    Update nfl_config.R

--- nfl_config.R
+++ nfl_config.R
@@ -1,5 +1,11 @@
 # local configuration hack
 
+
+if (Sys.info ()["user"] %in% c("thompson.bliss","Thompson.Bliss")) {
+ ## for Tom
+ base <- "C:/Users/Thompson.Bliss/National Football League/Lopez, Michael - Football Ops Analysis/"
+}
+
 if (Sys.info ()["user"] %in% c("michael.lopez","Michael.Lopez")) {
  ## for Mike
  base <- "C:/Users/Michael.Lopez/OneDrive - National Football League/Football Ops Analysis/"
@@ -14,9 +20,4 @@ if (Sys.info ()["user"] == "jack.persampire") {
 if (Sys.info ()["user"] == "sean.mckee") {
  ## for Sean
  base <- "C:/Users/Sean.McKee/OneDrive - National Football League/Football Ops Analysis/"
-}
-
-if (Sys.info ()["user"] %in% c("thompson.bliss","Thompson.Bliss")) {
- ## for Tom
- base <- "C:/Users/Thompson.Bliss/National Football League/Lopez, Michael - Football Ops Analysis/"
 }
